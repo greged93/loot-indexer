bdf38e9180d2ebcaabc2ddb889f3e4d548547bce
Gregory Edison <gregory.edison1993@gmail.com>
2023-09-03 16:19:42 +0200

    set up main

--- main.go
+++ main.go
@@ -2,11 +2,18 @@ package main
 
 import (
 	"fmt"
+	"loot-indexer/indexer"
 	"os"
+	"os/signal"
+	"strconv"
 	"syscall"
+	"time"
 
+	"github.com/NethermindEth/juno/core/felt"
 	"github.com/asynkron/protoactor-go/actor"
 	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 const (
@@ -14,8 +21,8 @@ const (
 )
 
 var (
-	indexer *actor.PID
-	done    = make(chan os.Signal)
+	idx  *actor.PID
+	done = make(chan os.Signal)
 )
 
 type GuardActor struct{}
@@ -23,7 +30,7 @@ type GuardActor struct{}
 func (guard *GuardActor) Receive(ctx actor.Context) {
 	switch ctx.Message().(type) {
 	case actor.Started:
-		ctx.Watch(indexer)
+		ctx.Watch(idx)
 	case actor.Terminated:
 		done <- syscall.SIGTERM
 	}
@@ -35,7 +42,65 @@ func main() {
 		fmt.Printf("failed to load env: %v", err)
 		os.Exit(1)
 	}
-	infuraKey := os.Getenv("URL_GOERLI")
+
+	infuraKey := loadVariable("INFURA_KEY")
 	url := URL_GOERLI + infuraKey
-	fmt.Printf("goerli url %s", url)
+
+	addr := loadVariable("CONTRACT_ADDRESS")
+	db := loadVariable("DB")
+	user := loadVariable("USER")
+	password := loadVariable("PASSWORD")
+	startBlock, err := strconv.ParseUint(loadVariable("START_BLOCK"), 10, 32)
+	if err != nil {
+		panic(err)
+	}
+
+	address, err := felt.Zero.SetString(addr)
+	if err != nil {
+		panic(err)
+	}
+
+	psqlInfo := fmt.Sprintf("host=localhost port=5432 user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		user, password, db)
+	gormDB, err := gorm.Open(postgres.Open(psqlInfo), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	cfg := indexer.NewIndexerConfig(url, address, startBlock, gormDB)
+
+	as := actor.NewActorSystem()
+	ctx := actor.NewRootContext(as, nil)
+
+	idx, err := ctx.SpawnNamed(actor.PropsFromProducer(indexer.NewIndexerProducer(cfg), actor.WithSupervisor(actor.NewExponentialBackoffStrategy(100*time.Second, time.Second))), "indexer")
+	if err != nil {
+		panic(err)
+	}
+
+	guard, err := ctx.SpawnNamed(actor.PropsFromProducer(func() actor.Actor { return &GuardActor{} }), "guard")
+	if err != nil {
+		panic(err)
+	}
+	signal.Notify(done, syscall.SIGINT, syscall.SIGTERM)
+	<-done
+	tries := 0
+	_ = ctx.PoisonFuture(guard).Wait()
+	for {
+		if err := ctx.PoisonFuture(idx).Wait(); err != nil {
+			break
+		}
+		tries++
+		if tries > 80 {
+			panic("failed to stop indexer")
+		}
+	}
+}
+
+func loadVariable(name string) string {
+	value := os.Getenv(name)
+	if value == "" {
+		panic(fmt.Sprintf("failed to load %s", name))
+	}
+	return value
 }
